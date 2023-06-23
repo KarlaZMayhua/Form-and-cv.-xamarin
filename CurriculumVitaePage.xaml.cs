20d09d3b506bbeb6ed6ccb83479db7adaab05961
KarlaZMayhua <113203880+KarlaZMayhua@users.noreply.github.com>
2023-06-23 15:39:12 -0500

    Add files via upload

---
+++ CurriculumVitaePage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Xamarin.Forms;
+
+namespace FormApp
+{
+    public partial class CurriculumVitaePage : ContentPage
+    {
+        public string NombresApellidos { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string Ocupacion { get; set; }
+        public string Aptitudes { get; set; }
+        public string CorreoElectronico { get; set; }
+        public string NumeroCelular { get; set; }
+
+        public CurriculumVitaePage()
+        {
+            InitializeComponent();
+            BindingContext = this;
+        }
+    }
+}
---
+++ FormPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using Xamarin.Forms;
+
+namespace FormApp
+{
+    public partial class FormPage : ContentPage
+    {
+        public FormPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Enviar_Clicked(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtNombres.Text) ||
+                string.IsNullOrWhiteSpace(txtApellidos.Text) ||
+                dpFechaNacimiento.Date == default ||
+                string.IsNullOrWhiteSpace(txtOcupacion.Text) ||
+                string.IsNullOrWhiteSpace(txtAptitudes.Text) ||
+                string.IsNullOrWhiteSpace(txtCorreoElectronico.Text) ||
+                string.IsNullOrWhiteSpace(txtNumeroCelular.Text))
+            {
+                await DisplayAlert("Error", "Debes completar todos los campos", "Aceptar");
+            }
+            else if (!IsValidEmail(txtCorreoElectronico.Text))
+            {
+                await DisplayAlert("Error", "Ingresa un correo electrónico válido", "Aceptar");
+            }
+            else
+            {
+                // Curriculum Vitae 
+                var cvPage = new CurriculumVitaePage
+                {
+                    Nombres = txtNombres.Text,
+                    Apellidos = txtApellidos.Text,
+                    FechaNacimiento = dpFechaNacimiento.Date,
+                    Ocupacion = txtOcupacion.Text,
+                    Aptitudes = txtAptitudes.Text,
+                    CorreoElectronico = txtCorreoElectronico.Text,
+                    NumeroCelular = txtNumeroCelular.Text
+                };
+                await Navigation.PushAsync(cvPage);
+            }
+        }
+
+        private bool IsValidEmail(string email)
+        {
+           //validacion c.e
+            return Regex.IsMatch(email, @"^\S+@\S+\.\S+$");
+        }
+    }
+}
